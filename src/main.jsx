8eb87046c329e3c18cef7bb69986066b4ee16274
Destiny Simmons <34193988+kismet-lee@users.noreply.github.com>
2022-07-13 23:05:14 +0900

    Fixed stylesheet source

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import "./css/output.css";
+import "./css/input.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
