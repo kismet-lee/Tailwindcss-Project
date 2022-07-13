9b85ec8ed408a49cb407845bd7342998afd3f6f7
Destiny Simmons <34193988+kismet-lee@users.noreply.github.com>
2022-07-13 22:53:32 +0900

    Trying to fix spacing issues
    Spacing issues are still not fixed at this point. Just poking around the code.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import "./css/input.css";
+import "./css/output.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
